9ec5d3f4aa4499b7e7f0a90eca60e4bbf3def8f4
= <=>
2023-06-29 11:44:07 +0530

    Added Rabbitmq setup

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,27 +1,40 @@
 const { BookingService } = require("../services/index");
 const { StatusCodes } = require("http-status-codes");
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
 
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    return res.status(StatusCodes.OK).json({
-      data: response,
-      success: true,
-      message: "Successfully created booking",
-      err: {},
-    });
-  } catch (error) {
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      data: [],
-      success: false,
-      message: "Some error occurred in booking controller",
-      err: "Some error occurred in booking controller",
+class BookingController {
+  constructor() {}
+
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = { message: "success" };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    return res.status(200).json({
+      message: "Successfully published the message",
     });
   }
-};
 
-module.exports = {
-  create,
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      return res.status(StatusCodes.OK).json({
+        data: response,
+        success: true,
+        message: "Successfully created booking",
+        err: {},
+      });
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        data: [],
+        success: false,
+        message: "Some error occurred in booking controller",
+        err: "Some error occurred in booking controller",
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { BookingController } = require("../../controllers/index");
-const router = express.Router();
 
-router.post("/book", BookingController.create);
+const router = express.Router();
+const bookingController = new BookingController();
+
+router.post("/book", bookingController.create);
+router.post("/publish", bookingController.sendMessageToQueue);
 
 module.exports = router;
